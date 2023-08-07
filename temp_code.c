1e03977610c751c4dbbe26e1696c3d0d54f75608
Soham Pande <sohampande747@gmail.com>
2023-08-08 01:21:27 +0530

    changes

--- temp_code.c
+++ temp_code.c
@@ -1,57 +1,22 @@
-#include <stdio.h>
 
-#define MAX_SIZE 10
-
-int main() {
-    int matA[MAX_SIZE][MAX_SIZE], matB[MAX_SIZE][MAX_SIZE], result[MAX_SIZE][MAX_SIZE];
-    int rowsA, colsA, rowsB, colsB;
-
-    // Input dimensions of matrix A
-    printf("Enter the number of rows and columns for matrix A: ");
-    scanf("%d %d", &rowsA, &colsA);
-
-    // Input elements of matrix A
-    printf("Enter the elements of matrix A:\n");
-    for (int i = 0; i < rowsA; i++) {
-        for (int j = 0; j < colsA; j++) {
-            scanf("%d", &matA[i][j]);
-        }
-    }
-
-    // Input dimensions of matrix B
-    printf("Enter the number of rows and columns for matrix B: ");
-    scanf("%d %d", &rowsB, &colsB);
-
-    // Input elements of matrix B
-    printf("Enter the elements of matrix B:\n");
-    for (int i = 0; i < rowsB; i++) {
-        for (int j = 0; j < colsB; j++) {
-            scanf("%d", &matB[i][j]);
-        }
-    }
-
-    // Perform matrix multiplication
-    if (colsA != rowsB) {
-        printf("Matrix multiplication is not possible.\n");
-    } else {
-        for (int i = 0; i < rowsA; i++) {
-            for (int j = 0; j < colsB; j++) {
-                result[i][j] = 0;
-                for (int k = 0; k < colsA; k++) {
-                    result[i][j] += matA[i][k] * matB[k][j];
-                }
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    int a,b;
+    *returnSize = 2;
+    int*ans = malloc(2*sizeof(int));
+    
+    for (int i = 0; i < numsSize-1; i++)
+    {
+        for (int j = i+1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                a = i;
+                b = j;
             }
         }
-
-        // Display the result matrix
-        printf("Result of matrix multiplication:\n");
-        for (int i = 0; i < rowsA; i++) {
-            for (int j = 0; j < colsB; j++) {
-                printf("%d ", result[i][j]);
-            }
-            printf("\n");
-        }
     }
-
-    return 0;
+    ans[0] = a;
+    ans[1] = b;
+    return ans;
 }
