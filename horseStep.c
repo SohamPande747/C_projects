bcbf66a66d598e1219668e225d9c1649178a9234
Soham Pande <sohampande747@gmail.com>
2023-11-14 08:55:03 +0530

    update AKodz

---
+++ horseStep.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int box[3][3];
+    int count = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            
+        }
+    }
+}
